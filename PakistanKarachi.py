1d5c11afe67ad9a2a8768dd4566021730213e2ab
Abdulbasitkamboh <55603039+Abdulbasitkamboh@users.noreply.github.com>
2020-09-04 21:01:03 +0500

    Update and rename KomailKhan.py to PakistanKarachi.py

--- PakistanKarachi.py
+++ PakistanKarachi.py
@@ -83,7 +83,7 @@ logo = """ ▄︻┻═┳一 ЩєLc๏Mє ┼๏ ┼ђє Fąş┼єş┼ єVєr
 \033[1;96mAuthor  \033[1;93m: \033[1;92mKomail Khan
 \033[1;96mInstagram \033[1;93m: \033[1: itx_muskan_yr
 \033[1;96mFacebook  \033[1;93m: \033[1: komail.khan.3781
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/KomailKhan/komailkhan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Abdulbasitkamboh/PAKISTAN
 \033[1;91m======================================="""
 
 def tik():
@@ -126,12 +126,12 @@ print  """\033[1;91m
 \033[1;96mAuthor  \033[1;93m: \033[1;92mKomail Khan
 \033[1;96mInstagram \033[1;93m: \033[1;92mitx_muskan_yr
 \033[1;96mFacebook  \033[1;93m: \033[1;92m komail.khan.3781
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/KomailKhan/komailkhan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Abdulbasitkamboh/PAKISTAN
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "komail"
-CorrectPassword = "khan"
+CorrectUsername = "ABDUL"
+CorrectPassword = "BASIT"
 
 loop = 'true'
 while (loop == 'true'):
